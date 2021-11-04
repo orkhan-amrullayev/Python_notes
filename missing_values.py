d2338a8272a272d113a2da53bbf68e9bf3304bce
Orkhan Amrullayev <64093617+orkhan-amrullayev@users.noreply.github.com>
2021-11-04 20:40:03 +0100

    Update missing_values.py

--- missing_values.py
+++ missing_values.py
@@ -1,6 +1,41 @@
 #####
 #####
 
+
+# checking the missing values of dataset
+
+df_new.isna().sum()
+
+
+#####
+#####
+
+
+# filling missing data with median of the column 
+# you can change it to mean as well
+
+for label, value in df_new.items():
+    
+    #in the dataset if any numeric data type value is null , fill it with the median of the column
+    
+    if pd.api.types.is_numeric_dtype(value):
+        if pd.isnull(value).sum():
+            df_new[label] = content.fillna(value.median())
+
+            
+#####
+#####
+
+
+#checking for leftover missing values
+#leftovers are categoricals, because they do not have any median to calculate, so they remained unchanged.
+df_new.isna().sum()
+
+
+
+#####
+#####
+
 # creating a list of those labels which have missing values
 # missing values
 
@@ -37,3 +72,15 @@ for label,value in df_new.items():
     if not pd.api.types.is_numeric_dtype(value):
         
         df_new[label] = pd.Categorical(value).codes+1
+
+        
+#####
+#####
+
+
+
+
+
+
+
+
