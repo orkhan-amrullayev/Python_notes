423c5f261a3927423574b4c70bd837b43741b0f1
Orkhan Amrullayev <64093617+orkhan-amrullayev@users.noreply.github.com>
2021-11-11 23:26:43 +0100

    Create encoding.py

---
+++ encoding.py
@@ -0,0 +1,32 @@
+###############
+##### OneHotEndcoding
+
+oneHot = pd.get_dummies(X[nominal_columns])
+X = pd.concat([X, oneHot], axis=1)
+X = X.drop(nominal_columns, axis=1)
+X.head()
+
+
+###############
+####  Custom map encoding
+
+# defining nominal columns manually
+
+# ordinal column
+ordinal_column1_map = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+}
+
+# binary column
+binary_column1 = {
+    'N': 0,
+    'Y': 1,
+}
+
+# maping the custom encoding
+
+X['ordinal_column'] = X.loan_grade.map(ordinal_column1_map)
+X.head()
